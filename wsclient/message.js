ca3b2dcb16322e596ec79ea02ba5d62708585bcf
Mattis Pasch <m.pasch@tarent.de>
2014-06-07 19:52:21 +0200

    stateKeeping agent beets default at 100%, './test_agent 100 default stateKeeping'

--- wsclient/message.js
+++ wsclient/message.js
@@ -1,7 +1,7 @@
 module.exports=function(data){
   var txt = data.toString();
   var matches = txt.match(/^(\d\d): (.+)$/);
-  var fieldMatches = txt.match(/[A-J]\d/);
+  var fieldMatches = txt.match(/([A-J]\d)/);
   return {
     code: parseInt(matches[1]),
     field: fieldMatches?fieldMatches[0]:null,
--- wsclient/test_agent.sh
+++ wsclient/test_agent.sh
@@ -4,13 +4,18 @@ touch wins1.log
 rm wins2.log
 touch wins2.log
 
-for i in `seq 1 10`;
+url="ws://localhost:40000/battle"
+times=$1
+agent1=$2
+agent2=$3
+
+for i in `seq 1 $times`;
 do
 
 
 echo "starting round $i";
 (
-  node index.js > out1.log
+  node index.js $url $agent1 > out1.log
   RETVAL=$?
   [ $RETVAL -eq 0 ] && (
       echo Success1; 
@@ -18,7 +23,7 @@ echo "starting round $i";
   )
   [ $RETVAL -ne 0 ] && echo Failure1
 ) & (
-  node index.js > out2.log
+  node index.js $url $agent2 > out2.log
   RETVAL=$?
   [ $RETVAL -eq 0 ] && (
       echo Success2; 
