805471d83e30ccc7ad048ad80fa1ccbce58ef6e6
Axel Dirla <a.dirla@tarent.de>
2014-06-08 17:02:23 +0200

    changed test_agent.js to solely listen redis-channels

--- wsclient/test_agent.js
+++ wsclient/test_agent.js
@@ -1,8 +1,3 @@
-process.on('SIGTERM', function(){
-    console.log('terminating');
-    process.exit(1);
-});
-
 var CommandLine = require("node-commandline").CommandLine;
 var commandLine = new CommandLine('test_agent.js');
 commandLine.addArgument('agent1',{type: 'string'})
@@ -11,7 +6,6 @@ commandLine.addArgument('agent1',{type: 'string'})
            .addArgument('serverUrl',{type: 'string'});
 
 
-//var command = commandLine.parseNode(process.argv[0],process.argv[1],process.argv[2],process.argv[3]);
 try{
   var command = commandLine.parseNode.apply(commandLine,process.argv);
 }catch( e ){
@@ -28,8 +22,6 @@ var repetitions = command.rep       || 100;
 
 var wins = [];
 var losses = [];
-var regLoss = 0;
-var regWin = 0;
 var roundsPlayed = 0;
 
 var redis = require('redis');
@@ -40,27 +32,27 @@ redisClient.on("error", function(err){
 });
 
 redisClient.on( "message", function(channel, message){ 
-debugger;
   console.log( "Channel '" + channel + "' published: " + message );
+
   if( channel === 'battleship_winner') { 
-    regWin++;  
     wins[message] = wins[message] ? (wins[message] + 1) : 1;
-  };
+    console.log('Rounds played',roundsPlayed); 
+    for( agent in wins ){ 
+      console.log(agent,': ', wins[agent]);
+    }
+  }
+
   if( channel === 'battleship_loser') { 
-    regLoss++;  
     losses[message] = losses[message] ? (losses[message] + 1) : 1;
-  };
-  if( regWin == 2 && regLoss == 1 && roundsPlayed < repetitions) {
     roundsPlayed++;
-    regWin = 0;
-    regLoss = 0;
-    startRound();
   }
 });
+
 
 redisClient.subscribe("battleship_winner");
 redisClient.subscribe("battleship_loser");
 
+/*
 var startRound = function(){
   var wsc1 = require('./ws-client')(serverUrl, agent1);
   var wsc2 = require('./ws-client')(serverUrl, agent2);
@@ -68,3 +60,4 @@ var startRound = function(){
 }
 
 startRound();
+*/
