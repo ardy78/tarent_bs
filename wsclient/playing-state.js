962eb562c7edaa244372d1ff860a1ad13b556229
Axel Dirla <a.dirla@tarent.de>
2014-06-08 21:35:02 +0200

    client kills himself if no move is down within a second in playing-state

--- wsclient/playing-state.js
+++ wsclient/playing-state.js
@@ -3,6 +3,9 @@ var defaultState = require("./default-state");
 var redis = require("redis");
 
 module.exports = function(emit, actions, states) {
+  
+
+  var firstMoveDone = false;
   var won = false; //hack to ignore second win-message
   var msgList = [];
   var dflt = defaultState(states);
@@ -17,6 +20,7 @@ module.exports = function(emit, actions, states) {
   var yourTurn = function() {
     // toEmit is a field,
     // TODO: implement special attacks
+    firstMoveDone = true;
     actions.attack(msgList, function(toEmit) {
       emit(toEmit);
     });
@@ -24,6 +28,15 @@ module.exports = function(emit, actions, states) {
   };
   return obj(dflt, {
     name: "playing",
+    enter: function(){
+      console.log('entered playing-state');
+      setTimeout( function(){
+          if( !firstMoveDone ){
+            process.exit(1);
+          }
+        }, 1000
+      );
+    },
     defaultAction: function(msg) {
       msgList.push(msg);
       if ([29, 31, 32].indexOf(msg.code) > -1) {
@@ -34,7 +47,7 @@ module.exports = function(emit, actions, states) {
     37: function() {
       redisClient.publish("battleship_loser",actions.name());
       emit("disconnect");
-      process.exit(1);
+      process.exit(0);
     },
     33: function() {
       if( !won ){
