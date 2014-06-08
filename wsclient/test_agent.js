2fe2b0a2de009fb39f77a9cd41506d6aafa7dd06
Axel Dirla <a.dirla@tarent.de>
2014-06-08 14:09:15 +0200

    extended test_agent.js to play a given number of rounds

--- wsclient/test_agent.js
+++ wsclient/test_agent.js
@@ -1,3 +1,8 @@
+process.on('SIGTERM', function(){
+    console.log('terminating');
+    process.exit(1);
+});
+
 var CommandLine = require("node-commandline").CommandLine;
 var commandLine = new CommandLine('test_agent.js');
 commandLine.addArgument('agent1',{type: 'string'})
@@ -21,19 +26,45 @@ var agent1      = command.agent1    || "default";
 var agent2      = command.agent2    || "default";
 var repetitions = command.rep       || 100;
 
+var wins = [];
+var losses = [];
+var regLoss = 0;
+var regWin = 0;
+var roundsPlayed = 0;
+
 var redis = require('redis');
 var redisClient = redis.createClient();
 
 redisClient.on("error", function(err){
-  console.log( "Couldn't create redis client" );
+  console.log( "Couldn't create redis client: " + err);
 });
 
 redisClient.on( "message", function(channel, message){ 
+debugger;
   console.log( "Channel '" + channel + "' published: " + message );
+  if( channel === 'battleship_winner') { 
+    regWin++;  
+    wins[message] = wins[message] ? (wins[message] + 1) : 1;
+  };
+  if( channel === 'battleship_loser') { 
+    regLoss++;  
+    losses[message] = losses[message] ? (losses[message] + 1) : 1;
+  };
+  if( regWin == 2 && regLoss == 1 && roundsPlayed < repetitions) {
+    roundsPlayed++;
+    regWin = 0;
+    regLoss = 0;
+    startRound();
+  }
 });
 
 redisClient.subscribe("battleship_winner");
 redisClient.subscribe("battleship_loser");
 
-require('./ws-client')(serverUrl, agent1);
-require('./ws-client')(serverUrl, agent2);
+var startRound = function(){
+  var wsc1 = require('./ws-client')(serverUrl, agent1);
+  var wsc2 = require('./ws-client')(serverUrl, agent2);
+  console.log('wsc1+2: ',wsc1,wsc2);
+}
+
+startRound();
