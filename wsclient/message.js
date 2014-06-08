aa6240dee9f5762c2ed0c7c7585f403aa044cd0c
Axel Dirla <a.dirla@tarent.de>
2014-06-08 03:54:23 +0200

    Made message.js more robust

--- wsclient/message.js
+++ wsclient/message.js
@@ -2,9 +2,15 @@ module.exports=function(data){
   var txt = data.toString();
   var matches = txt.match(/^(\d\d): (.+)$/);
   var fieldMatches = txt.match(/[A-J]\d/);
+  var c;
+  try{
+    c=parseInt(matches[1]);
+  }catch(e){
+    //?
+  }
   return {
-    code: parseInt(matches[1]),
-    field: fieldMatches?fieldMatches[0]:null,
-    txt:matches[2]
+    code: c ? c : null,
+    field: fieldMatches ? fieldMatches[0] : null,
+    txt: (matches && (matches.length > 2)) ? matches[2] : null
   };
 };
