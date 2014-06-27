99f0a4d1292b5416c9717af2bccbc7605f71f04d
Axel Dirla <a.dirla@tarent.de>
2014-06-27 16:25:14 +0200

    removed redis-specific code from playing-state.js

--- wsclient/playing-state.js
+++ wsclient/playing-state.js
@@ -1,6 +1,5 @@
 var obj = require("./utils").object;
 var defaultState = require("./default-state");
-var redis = require("redis");
 
 module.exports = function(emit, actions, states) {
   
@@ -8,14 +7,8 @@ module.exports = function(emit, actions, states) {
   var won = false; //hack to ignore second win-message
   var msgList = [];
   var dflt = defaultState(states);
-  var redisClient;
   var executingSpecialMove=false;
-  redisClient = redis.createClient();
 
-  redisClient.on("error", function (err) {
-    console.log("Error " + err);
-    console.log('redisClient', redisClient );
-  });
 
   var yourTurn = function() {
     // toEmit is a field,
@@ -52,20 +45,5 @@ module.exports = function(emit, actions, states) {
       executingSpecialMove=false;
       yourTurn();
     },
-   /*
-    37: function() {
-      redisClient.publish("battleship_loser",actions.name());
-      //emit("disconnect");
-      //process.exit(0);
-    },
-    33: function() {
-      if( !won ){
-        won = true;
-        redisClient.publish("battleship_winner",actions.name());
-        //emit("disconnect");
-        //process.exit(0);
-      }
-    }
-   */
   });
 };
